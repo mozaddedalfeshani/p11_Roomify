ac23ac39438a5d814a5a983492221db44b3b2fe7
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-22 22:35:32 +0600

    Add Google sign-in functionality to AuthProvider; implement googleSignIn method

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,8 @@
 import React, { createContext, useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../firebase/firebase.init";
 
+const provider = new GoogleAuthProvider();
 //create auth context
 
 const AuthContext = createContext();
@@ -10,10 +13,22 @@ const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
 
   // function to sign in with Google
+  const googleSignIn = async () => {
+    const auth = getAuth(app);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   const data = {
-    // user,
-    // googleSignIn,
+    user,
+    setUser,
+    googleSignIn,
   };
 
   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
