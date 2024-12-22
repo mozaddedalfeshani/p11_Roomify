1502d0e6401c0c4c742c76f40618062da9b14060
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-12-22 20:58:44 +0600

    Fix FeaturedRoom component to use axios for fetching room data and update HOST URL format

--- src/Components/Home/FeaturedRoom.jsx
+++ src/Components/Home/FeaturedRoom.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { HOST } from "../../host";
-import { data } from "react-router-dom";
 
 const FeaturedRoom = () => {
-  const [rooms, setRooms] = useState([21]);
-
+  const [rooms, setRooms] = useState([]);
+  console.log(HOST);
   useEffect(() => {
     const fetchRooms = async () => {
-      fetch("http://localhost:9000/rooms")
-        .then((res) => res.json())
-        .then((data) => setRooms(data));
+      axios
+        .get(`${HOST}/rooms`)
+
+        .then((res) => {
+          console.log(res.data);
+          setRooms(res.data);
+        });
     };
     fetchRooms();
   }, []);
 
-  return <div className="my-6">{rooms.length}</div>;
+  return <div className="my-6">Rooms number : {rooms.length}</div>;
 };
 
 export default FeaturedRoom;
